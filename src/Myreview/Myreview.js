006469146938238a018bb4fc38de56fcc09e5a89
mdshahjhan <mdshahjha478877@gmail.com>
2022-11-09 18:59:55 +0600

    add

--- src/Myreview/Myreview.js
+++ src/Myreview/Myreview.js
@@ -1,17 +1,18 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { AuthContext } from '../Context/AuthProvider';
 import ReviewAdd from '../ReviewAdd/ReviewAdd';
 
 const Myreview = () => {
     const service = useLoaderData()
     const {user} = createContext(AuthContext) 
-   const [myreview,setMyreview] = useState([])
+   const [review,setReview] = useState([])
 
    useEffect(()=>{
     fetch(`http://localhost:5000/Orders?email${user?.email}`)
     .then(res=>res.json())
-    .then(data=>setMyreview(data))
+    .then(data=>setReview(data))
    },[user?.email])
 
    const handleDelete=id=>{
@@ -24,14 +25,35 @@ const Myreview = () => {
             .then(data=>{
                 console.log(data)
                 if(data.deleteCount>0){
-                    alert('deleted successfullay')
-                    const remaining = myreview.filter(odr => odr._id !== id);
-                    setMyreview(remaining);
+                    toast.success('successfullay delete',{autoClose:500})
+                    const remaining =review.filter(review => review._id !== id);
+                    setReview(remaining);
                 }
             })
     }
 
    } 
+   const handleStatusUpdate = id =>{
+    fetch(`https://y-phi-indol.vercel.app/orders/${id}`, {
+        method: 'PATCH', 
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({status: 'Approved'})
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        if(data.modifiedCount > 0) {
+            const remaining = review.filter(review =>review._id !== id);
+            const approving = review.find(review => review._id === id);
+            approving.status = 'Approved'
+            const newOrders = [approving, ...remaining];
+            setReview(newOrders);
+        }
+    })
+}
+
 
     return (
         <div>
@@ -42,7 +64,8 @@ const Myreview = () => {
                 {
                     service.map(service=><ReviewAdd key={service._id}
                     service={service}
-                    handleDelete={handleDelete}>
+                    handleDelete={handleDelete}
+                    handleStatusUpdate={handleStatusUpdate}>
 
                     </ReviewAdd>)
                 }
--- src/ReviewAdd/ReviewAdd.js
+++ src/ReviewAdd/ReviewAdd.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import Myreview from '../Myreview/Myreview';
 
 const ReviewAdd = ({service}) => {
-    const { img, name, short_description,price,_id} = service
+    const { img, name, short_description,price,_id,} = service
     const [review,setReview] = useState([])
+
+
     const handleDelete =(id)=>{
         const proceed = window.confirm('are you sure you want to orders delete button')
     if(proceed){
@@ -13,14 +17,36 @@ const ReviewAdd = ({service}) => {
             .then(data=>{
                 console.log(data)
                 if(data.deleteCount>0){
-                    alert('deleted successfullay')
-                    const remaining = review.filter(rodr => rodr._id !== id);
+                    toast.success('successfullay delete',{autoClose:500})
+                    const remaining = review.filter(review => review._id !== id);
                     setReview(remaining);
                 }
             })
         }
     }
 
+    const handleStatusUpdate=id =>{
+        fetch(`https://y-phi-indol.vercel.app/orders/${id}`, {
+            method: 'PATCH', 
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({status: 'Approved'})
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.modifiedCount > 0) {
+                toast.success('successfullay update',{autoClose:500})
+                const remaining = review.filter(review =>review._id !== id);
+                const approving = review.find(review => review._id === id);
+                approving.status = 'Approved'
+                const newOrders = [approving, ...remaining];
+                setReview(newOrders);
+            }
+        })
+    }
+    
     return (
         <div>
             
@@ -41,11 +67,11 @@ const ReviewAdd = ({service}) => {
         </button>
         <br></br>
         <br></br>
-        <button>
-        <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <button onClick={()=>handleStatusUpdate(_id)}>
+        <div href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Update
             <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-        </a>
+        </div>
         </button>
     </div>
 </div>
