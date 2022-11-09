c60878b065a3464e9518789a72ce2f159cf04f8c
mdshahjhan <mdshahjha478877@gmail.com>
2022-11-09 10:16:40 +0600

    nodejs

--- src/Blog/Blog.js
+++ src/Blog/Blog.js
@@ -12,11 +12,11 @@ const Blog = () => {
       
       <p class="mb-3 font-light text-gray-500 dark:text-gray-400">
         <h1 className='text-3xl'>What is JWT, and how does it work?</h1>
-        <p className='text-2xl text-green-300'>What is JWT (JSON Web Token)? JSON Web Token (JWT) is an open standard (RFC 7519) for securely transmitting information between parties as JSON object. It is compact, readable and digitally signed using a private key/ or a public key pair by the Identity Provider(IdP).</p>
+        <p className='text-2xl text-red-600'>What is JWT (JSON Web Token)? JSON Web Token (JWT) is an open standard (RFC 7519) for securely transmitting information between parties as JSON object. It is compact, readable and digitally signed using a private key/ or a public key pair by the Identity Provider(IdP).</p>
        </p>
       <p class="mb-3 font-light text-gray-500 dark:text-gray-400">
-        <h1 className='text-3xl'></h1>
-        <p className='text-2xl text-green-300'>.</p>
+        <h1 className='text-3xl'>What is the difference between javascript and NodeJS?</h1>
+        <p className='text-2xl text-orange-500'>JavaScript is a simple programming language that can be used with any browser that has the JavaScript Engine installed. Node. js, on the other hand, is an interpreter or execution environment for the JavaScript programming language..</p>
        </p>
       
 
