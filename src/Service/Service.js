9b75f799e13321aeae319a726d2cfa1224bce85e
mdshahjhan <mdshahjha478877@gmail.com>
2022-11-10 18:17:14 +0600

    update

--- src/Service/Service.js
+++ src/Service/Service.js
@@ -18,7 +18,7 @@ const Service = () => {
             <div className='text-center mb-4'>
                 <p className="text-2xl font-bold text-red-400">Services</p>
             </div>
-            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mx-18 my-18'>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mx-20 my-20'>
                 {
                     service.map(service => <ServiceCard key={service._id} service={service}
                         handleshow={handleshow}>
--- src/Showall/Showall.js
+++ src/Showall/Showall.js
@@ -22,7 +22,7 @@ const Showall = () => {
             <div className='text-center mb-4'>
                 <p className="text-2xl font-bold text-red-400">Services</p>
             </div>
-            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mx-18 my-18'>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mx-20 my-20'>
                 {
                     service.map(service => <ServiceShowAll key={service._id}
                         service={service}>
