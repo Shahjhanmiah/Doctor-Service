81b7c6990f7afcfbba8afdd1401ac84c5f5c8208
mdshahjhan <mdshahjha478877@gmail.com>
2022-12-13 18:05:15 +0600

    new branch

--- src/Service/Service.js
+++ src/Service/Service.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import ServiceCard from '../ServiceCard/ServiceCard';
 
 const Service = () => {
@@ -17,6 +18,7 @@ const Service = () => {
         <div>
             <div className='text-center mb-4'>
                 <p className="text-2xl font-bold text-red-400">Services</p>
+
             </div>
             <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mx-20 my-20'>
                 {
@@ -26,9 +28,13 @@ const Service = () => {
                     </ServiceCard>)
                 }
 
-                
+
+
 
             </div>
+            <Link to='/showall'>
+                <button className="btn btn-primary">Seall</button>
+            </Link>
         </div>
     );
 };
